23b162ebd0725e9b8d6cfd90c041a31ff340e18f
hougangdev <devchain7890@gmail.com>
2024-10-12 02:04:28 +0800

    add fix contract

---
+++ src/MerkleAirdrop.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import { IERC20, SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+contract MerkleAirdrop {
+}
